0c0ecc846c88c468fb960307e4acd35c1fb71c50
BlueLion111 <2365735828qq.com>
2026-06-28 09:32:21 +0800

    day2 task2：完善7个页面路由系统

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,10 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/',
+      redirect: '/home',
+    },
+    {
       path: '/home',
       name: 'Home',
       component: () => import('../views/HomeView.vue'),
